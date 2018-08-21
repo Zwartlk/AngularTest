606f95b92b30dd2a65a17f4acc67b4582f5512d6
hntlk <whitetrang1995@gmail.com>
2018-08-22 00:19:26 +0700

    commit 003

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { ErrorHandler } from '@angular/core';
 import { AppErrorHandler } from './common/app-error-handler';
 import { HttpModule } from '@angular/http';
@@ -7,17 +8,34 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { PostsComponent } from './posts/posts.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GithubprofileComponent } from './githubprofile/githubprofile.component';
+import { RouterModule } from '@angular/router';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    PostsComponent
+    PostsComponent,
+    NavBarComponent,
+    GithubprofileComponent,
+    GithubFollowersComponent,
+    NotFoundPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path:'', component:HomeComponent },
+      { path:'followers', component:GithubFollowersComponent },
+      { path:'followers/:username', component:GithubprofileComponent },
+      { path:'posts', component:PostsComponent },
+      { path:'**', component:NotFoundPageComponent },
+    ])
   ],
   providers: [    
     {provide:ErrorHandler, useClass:AppErrorHandler}
